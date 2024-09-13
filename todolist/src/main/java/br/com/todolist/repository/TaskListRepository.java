1ef5e37cbe4c288ade8aaff86fd8b25d72288dc3
Feitosa <feitosawj@gmail.com>
2024-09-13 16:00:55 -0300

    FEAT- implementando a pesquisa da Lista de tarefas ordenada no TaskListRepository

--- todolist/src/main/java/br/com/todolist/repository/TaskListRepository.java
+++ todolist/src/main/java/br/com/todolist/repository/TaskListRepository.java
@@ -3,5 +3,9 @@ package br.com.todolist.repository;
 import br.com.todolist.model.TaskList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskListRepository extends JpaRepository<TaskList, Long> {
+
+    List<TaskList> findAllByOrderByCreationDateDesc();
 }
