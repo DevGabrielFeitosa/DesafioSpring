4412f25d082dc55fe623a4cfff80ff9b226e9b43
Feitosa <feitosawj@gmail.com>
2024-09-13 15:40:42 -0300

    FEAT- implementando busca com ordenação no TaskRepository

--- todolist/src/main/java/br/com/todolist/repository/TaskRepository.java
+++ todolist/src/main/java/br/com/todolist/repository/TaskRepository.java
@@ -1,7 +1,12 @@
 package br.com.todolist.repository;
 
 import br.com.todolist.model.Task;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findAllWithOrder(Sort sort);
 }
