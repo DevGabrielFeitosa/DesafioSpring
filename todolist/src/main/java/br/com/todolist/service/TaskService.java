c4ff00f150a792d916759a455f1e238f27deac2b
Feitosa <feitosawj@gmail.com>
2024-09-13 15:40:06 -0300

    FEAT- implementando CRUD no TaskService

--- todolist/src/main/java/br/com/todolist/service/TaskService.java
+++ todolist/src/main/java/br/com/todolist/service/TaskService.java
@@ -1,7 +1,40 @@
 package br.com.todolist.service;
 
+import br.com.todolist.model.Task;
+import br.com.todolist.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> createTask(Task task){
+        taskRepository.save(task);
+        return findAll();
+    }
+
+    public List<Task> findAll(){
+        return taskRepository.findAll();
+    }
+
+    public List<Task> findAllWithOrder(String order){
+        Sort sort = Sort.by(Sort.Order.asc(order));
+        return taskRepository.findAllWithOrder(sort);
+    }
+
+    public List<Task> deleteTask(Long id){
+        taskRepository.deleteById(id);
+        return findAll();
+    }
+
+    public List<Task> updateTask(Task task){
+        taskRepository.save(task);
+        return findAll();
+    }
+
 }
