c4a86c00d4eaae2371832b8f32e3a1ef639a141a
Feitosa <feitosawj@gmail.com>
2024-09-13 12:31:31 -0300

    FEAT- implementando o model Task

---
+++ todolist/src/main/java/br/com/todolist/model/Task.java
@@ -0,0 +1,61 @@
+package br.com.todolist.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+
+    private String description;
+
+    private String priority;
+
+    private Date creationDate;
+
+    private String status;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
