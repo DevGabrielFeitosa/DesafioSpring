06cc1dda921d7d074da9c92dd5af2df951178eed
Feitosa <feitosawj@gmail.com>
2024-09-13 16:09:00 -0300

    FEAT- implementando o TaskListController para expor as apis e receber as requisições

---
+++ todolist/src/main/java/br/com/todolist/controller/TaskListController.java
@@ -0,0 +1,35 @@
+package br.com.todolist.controller;
+
+import br.com.todolist.model.TaskList;
+import br.com.todolist.service.TaskListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/task")
+public class TaskListController {
+    @Autowired
+    private TaskListService taskListService;
+
+    @PostMapping
+    List<TaskList> create(@RequestBody TaskList taskList){
+        return taskListService.createList(taskList);
+    }
+
+    @GetMapping
+    List<TaskList> list(){
+        return taskListService.findAll();
+    }
+
+    @PutMapping
+    List<TaskList> update(@RequestBody TaskList taskList){
+        return taskListService.updateList(taskList);
+    }
+
+    @DeleteMapping("{id}")
+    List<TaskList> delete(@PathVariable("id") Long id){
+        return taskListService.deleteList(id);
+    }
+}
