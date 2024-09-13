e52db7830a3f910e44586101f0a5ca186fe19a2d
Feitosa <feitosawj@gmail.com>
2024-09-13 17:41:08 -0300

    FIX - trocando o tipo do ID para UUID

--- todolist/src/main/java/br/com/todolist/repository/TaskListRepository.java
+++ todolist/src/main/java/br/com/todolist/repository/TaskListRepository.java
@@ -4,8 +4,9 @@ import br.com.todolist.model.TaskList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.UUID;
 
-public interface TaskListRepository extends JpaRepository<TaskList, Long> {
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
 
     List<TaskList> findAllByOrderByCreationDateDesc();
 }
