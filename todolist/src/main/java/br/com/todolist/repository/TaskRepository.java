baa256bb245141b30c72d7c423ac734d1c96fdec
Feitosa <feitosawj@gmail.com>
2024-09-13 17:41:19 -0300

    FIX - trocando o tipo do ID para UUID e removendo método desnecessário

--- todolist/src/main/java/br/com/todolist/repository/TaskRepository.java
+++ todolist/src/main/java/br/com/todolist/repository/TaskRepository.java
@@ -1,12 +1,9 @@
 package br.com.todolist.repository;
 
 import br.com.todolist.model.Task;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.UUID;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
-
-    List<Task> findAllWithOrder(Sort sort);
+public interface TaskRepository extends JpaRepository<Task, UUID> {
 }
