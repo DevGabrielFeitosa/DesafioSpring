ed00b69d69c77c2fd04214484ba7b7493f608163
Feitosa <feitosawj@gmail.com>
2024-09-13 22:49:35 -0300

    FEAT - implementando Exception Handler para capturar exceções da classe TaskList

---
+++ todolist/src/main/java/br/com/todolist/exceptions/TaskListExceptionHandler.java
@@ -0,0 +1,28 @@
+package br.com.todolist.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class TaskListExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleModelExceptions(MethodArgumentNotValidException ex) {
+        Map<String,String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) ->{
+            String key = ((FieldError) error).getField();
+            String value = error.getDefaultMessage();
+
+            errors.put(key,value);
+        });
+
+        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
+    }
+}
