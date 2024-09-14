164256db2ef36aac3a43bffebe93986201e0382b
Feitosa <feitosawj@gmail.com>
2024-09-14 17:17:53 -0300

    FIX- implementando o formato de data esperado

--- todolist/src/main/java/br/com/todolist/DTO/TaskDTO.java
+++ todolist/src/main/java/br/com/todolist/DTO/TaskDTO.java
@@ -1,5 +1,7 @@
 package br.com.todolist.DTO;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 import java.util.UUID;
 
@@ -7,6 +9,7 @@ public class TaskDTO {
     private UUID id;
     private String description;
     private String priority;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Date creationDate;
     private String status;
 
