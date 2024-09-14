03dd5fba3d606682cec283e9a27ee16de36ef2d6
Feitosa <feitosawj@gmail.com>
2024-09-14 16:52:10 -0300

    TEST- implementando novos casos de teste para o TaskService

--- todolist/src/main/java/br/com/todolist/exceptions/TaskExceptionHandler.java
+++ todolist/src/main/java/br/com/todolist/exceptions/TaskExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +13,13 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class TaskExceptionHandler {
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<Map<String, String>> handleMissingParams(MissingServletRequestParameterException ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put(ex.getParameterName(), "Parâmetro ausente.");
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleModelExceptions(MethodArgumentNotValidException ex) {
---
+++ todolist/src/test/java/br/com/todolist/TaskServiceTest.java
@@ -0,0 +1,208 @@
+package br.com.todolist;
+
+import br.com.todolist.DTO.TaskDTO;
+import br.com.todolist.controller.TaskController;
+import br.com.todolist.model.TaskListModel;
+import br.com.todolist.model.TaskModel;
+import br.com.todolist.repository.TaskRepository;
+import br.com.todolist.service.TaskListService;
+import br.com.todolist.service.TaskService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(TaskController.class)
+public class TaskServiceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TaskService taskService;
+
+    @MockBean
+    private TaskRepository taskRepository;
+
+    @MockBean
+    private TaskListService taskListService;
+
+    private UUID validTaskListId;
+
+    @BeforeEach
+    public void setUp() throws ParseException {
+        validTaskListId = UUID.randomUUID();
+        TaskListModel taskListModel = new TaskListModel();
+
+        when(taskListService.findById(validTaskListId)).thenReturn(taskListModel);
+    }
+
+    @Test
+    public void shouldCreateNewTask() throws Exception {
+        String taskJson = "{\"description\": \"Teste para verificar se está tudo ok\", \"priority\": \"High\", \"creationDate\": \"2024-09-14 15:30:03\", \"status\": \"Open\"}";
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date creationDate = sdf.parse("2024-09-14 15:30:03");
+
+        TaskDTO createdTask = new TaskDTO();
+        createdTask.setDescription("Teste para verificar se está tudo ok");
+        createdTask.setPriority("High");
+        createdTask.setCreationDate(creationDate);
+        createdTask.setStatus("Open");
+
+        mockMvc.perform(post("/task")
+                        .param("taskListId", validTaskListId.toString())
+                        .contentType(APPLICATION_JSON)
+                        .content(taskJson))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldReturnBadRequestWhenTaskListIdIsMissing() throws Exception {
+        String taskJson = "{\"description\": \"Teste para verificar se está tudo ok\", \"priority\": \"High\", \"creationDate\": \"2024-09-14 15:30:03\", \"status\": \"Open\"}";
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date creationDate = sdf.parse("2024-09-14 15:30:03");
+
+        TaskDTO createdTask = new TaskDTO();
+        createdTask.setDescription("Teste para verificar se está tudo ok");
+        createdTask.setPriority("High");
+        createdTask.setCreationDate(creationDate);
+        createdTask.setStatus("Open");
+
+        mockMvc.perform(post("/task")
+                        .contentType(APPLICATION_JSON)
+                        .content(taskJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.taskListId").value("Parâmetro ausente."));
+    }
+
+    @Test
+    public void shouldReturnBadRequestWhenDescriptionIsBlank() throws Exception {
+        String taskJson = "{\"description\": \"\", \"priority\": \"High\", \"creationDate\": \"2024-09-14 15:30:03\", \"status\": \"Open\"}";
+
+        mockMvc.perform(post("/task")
+                        .contentType(APPLICATION_JSON)
+                        .content(taskJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.description").value("Descrição não deve ser vazia."));
+    }
+
+    @Test
+    public void shouldReturnBadRequestWhenDescriptionIsTooLong() throws Exception {
+        String taskJson = "{\"description\": \"" + "A".repeat(201) + "\", \"priority\": \"High\", \"creationDate\": \"2024-09-14 15:30:03\", \"status\": \"Open\"}";
+
+        mockMvc.perform(post("/task")
+                        .contentType("application/json")
+                        .content(taskJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.description").value("Descrição não deve possuir mais que 200 caracteres."));
+    }
+
+    @Test
+    public void shouldReturnListOfTaks() throws Exception {
+        TaskDTO task1 = new TaskDTO();
+        task1.setDescription("Task 1");
+        task1.setPriority("High");
+        task1.setCreationDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2024-09-14 15:30:03"));
+        task1.setStatus("Open");
+
+        TaskDTO task2 = new TaskDTO();
+        task2.setDescription("Task 2");
+        task2.setPriority("Low");
+        task2.setCreationDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2024-09-14 15:30:03"));
+        task2.setStatus("Closed");
+
+        when(taskService.findAll()).thenReturn(Arrays.asList(task1, task2));
+
+        mockMvc.perform(get("/task/list")
+                        .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].description").value("Task 1"))
+                .andExpect(jsonPath("$[1].description").value("Task 2"));
+
+
+        mockMvc.perform(get("/task/list")
+                        .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].description").value("Task 1"))
+                .andExpect(jsonPath("$[1].description").value("Task 2"));
+    }
+
+    @Test
+    public void shouldReturnOrderedListOfTasks() throws Exception {
+        TaskDTO task1 = new TaskDTO();
+        task1.setDescription("Task 3");
+        task1.setPriority("1");
+        task1.setCreationDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2024-09-14 15:30:03"));
+        task1.setStatus("Open");
+
+        TaskDTO task2 = new TaskDTO();
+        task2.setDescription("Task 1");
+        task2.setPriority("0");
+        task2.setCreationDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2024-09-14 15:30:03"));
+        task2.setStatus("Closed");
+
+        TaskDTO task3 = new TaskDTO();
+        task3.setDescription("Task 2");
+        task3.setPriority("2");
+        task3.setCreationDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2024-09-14 15:30:03"));
+        task3.setStatus("Closed");
+
+        when(taskService.findAllWithOrder("priority")).thenReturn(Arrays.asList(task2, task1, task3));
+
+        mockMvc.perform(get("/task/list/orderedList")
+                        .param("order", "priority")
+                        .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].description").value("Task 1"))
+                .andExpect(jsonPath("$[1].description").value("Task 3"))
+                .andExpect(jsonPath("$[2].description").value("Task 2"));
+    }
+
+    @Test
+    public void shouldDeleteWithSuccess() throws Exception {
+        UUID idTask1 = UUID.randomUUID();
+        UUID idTask2 = UUID.randomUUID();
+
+        TaskDTO task1 = new TaskDTO();
+        task1.setId(idTask1);
+        task1.setDescription("Task 1");
+        task1.setPriority("High");
+        task1.setCreationDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2024-09-14 15:30:03"));
+        task1.setStatus("Open");
+
+        TaskDTO task2 = new TaskDTO();
+        task2.setId(idTask2);
+        task2.setDescription("Task 2");
+        task2.setPriority("Low");
+        task2.setCreationDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2024-09-14 15:30:03"));
+        task2.setStatus("Closed");
+
+        when(taskService.findAll()).thenReturn(Arrays.asList(task1, task2));
+
+        when(taskService.deleteTask(idTask2)).thenReturn(Arrays.asList(task1));
+
+        mockMvc.perform(delete("/task/{id}", idTask2)
+                        .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].description").value("Task 1"));
+    }
+}
